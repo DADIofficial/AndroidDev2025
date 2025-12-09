6bd7299a313e83d71a5a81838d9fa4a5aa928673
DADIofficial <tipodosym@gmail.com>
2025-12-09 10:13:41 +0500

    Add Nav and first GameFragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,11 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.navigation:navigation-fragment-ktx:<version>")
+    implementation("androidx.navigation:navigation-ui-ktx:<version>")
 }
